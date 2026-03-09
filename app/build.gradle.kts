380161eb0766f929d42c712d602ab438800e04ac
Shejan <beingshejanahmmed@gmail.com>
2026-03-10 02:31:08 +0600

    Update aad File

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shejan.musicbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
