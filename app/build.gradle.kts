3fb7a8840c4b4110f844e608930748a1d901c9c0
Shejan <beingshejanahmmed@gmail.com>
2026-03-10 02:16:03 +0600

    UI: Refine component contrast, standardize borderless layout, adjust list spacing, optimize dark/light mode themes, and bump version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shejan.musicbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.4.0"
+        versionCode = 12
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
