b3da66d7de9398dee518b71da0b633060f03f1f3
Shejan <beingshejanahmmed@gmail.com>
2026-06-20 03:28:26 +0600

    feat(settings): introduce experimental widgets toggle and bump version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 20
-        versionName = "2.0.0"
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
