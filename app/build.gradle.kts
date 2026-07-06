d4e3795a0c8bd132018f299c76f78b0b99bcd89e
Shejan <beingshejanahmmed@gmail.com>
2026-07-07 04:56:46 +0600

    ui: refine artwork border styling and bump version to 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shejan.musicbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 22
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
