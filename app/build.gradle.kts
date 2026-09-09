de5480335904c16258b6ad527e8328ac2a027b22
Shejan <beingshejanahmmed@gmail.com>
2026-09-10 03:18:43 +0600

    chore(release): bump version to 2.5.0 (build 25) and enable R8 full mode optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shejan.musicbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "2.4.0"
+        versionCode = 25
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
