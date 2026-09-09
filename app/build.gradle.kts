7c461c4acff6e4b0d92ac90632211ebab08e40a3
Shejan <beingshejanahmmed@gmail.com>
2026-09-10 04:29:15 +0600

    chore(release): bump version to 2.6.0 (26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shejan.musicbox"
         minSdk = 24
         targetSdk = 36
-        versionCode = 25
-        versionName = "2.5.0"
+        versionCode = 26
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
